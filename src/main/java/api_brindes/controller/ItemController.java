1bb134d5c499d13964a894c26ec0f23928570afc
Leandro <leoleando3478@gmail.com>
2026-08-18 19:08:45 -0300

    Feat: Adicionando edicao de valores unitarios e formatacao monetaria padrao BRL

--- src/main/java/api_brindes/controller/ItemController.java
+++ src/main/java/api_brindes/controller/ItemController.java
@@ -5,6 +5,9 @@ import api_brindes.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 import java.util.HashMap;
 import java.util.List;
@@ -44,7 +47,7 @@ public class ItemController {
 
         // A MÁGICA ACONTECE AQUI: Nomes idênticos ao seu HTML!
         relatorio.put("totalDeBrindesCadastrados", quantidadeTotal);
-        relatorio.put("valorTotalArmazenado", String.format("R$ %.2f", patrimonioTotal));
+        relatorio.put("valorTotalArmazenado", patrimonioTotal);
 
         return ResponseEntity.ok(relatorio);
     }
@@ -58,5 +61,17 @@ public class ItemController {
         }
         return ResponseEntity.notFound().build();
     }
+    @PutMapping("/{id}")
+    public ResponseEntity<Item> atualizarValor(@PathVariable Integer id, @RequestBody Item itemAtualizado) {
+        // Trocamos Long por Integer e colocamos itemRepository
+        Item itemExistente = itemRepository.findById(id).orElseThrow();
+
+        // Atualiza apenas o valor unitário
+        itemExistente.setValor(itemAtualizado.getValor());
+
+        itemRepository.save(itemExistente);
+        return ResponseEntity.ok(itemExistente);
+    }
+
 
 }
